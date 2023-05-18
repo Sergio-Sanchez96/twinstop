d11c2bf4d74b04407dc3aee29677d9ed14d02b06
Sergio Sanchez Moragues <ssanchez@drac>
2023-05-18 14:56:46 +0200

    Package updated on 18/05/23

---
+++ src/twinstop/__init__.py
@@ -0,0 +1,5 @@
+from ._version import __version__
+from .denovo_selenos import main as run_twinstop
+from .blastp_heatmap import main as run_blastp
+from .test_alignment_methods import *
+from .block_selection import *
